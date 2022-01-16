3a0f1b5d07781d00d3c484f61b7425aba80e71c5
Joel VG <cgjavg@gmail.com>
2022-01-16 00:31:42 -0400

    Set urls for views

--- service/bonds/urls.py
+++ service/bonds/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from bonds.views import BondsViewSet
 
-
 detail = BondsViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})
 buy = BondsViewSet.as_view({'get': 'buy','put': 'buy'})
 index = BondsViewSet.as_view({'get': 'list', 'post': 'create'})
@@ -10,5 +9,4 @@ urlpatterns = [
     path('bonds', index, name='bonds-list'),
     path('bonds/<str:pk>', detail, name='bonds-detail'),
     path('bonds/<str:pk>/buy', buy, name='bonds-buy'),
-    # path('bonds/^purchases/USD', buy, name='bonds-buy'),
 ]
